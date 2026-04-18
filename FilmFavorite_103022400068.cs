1c94df69278baeaf0a714dd4a88fc55c3de3c07a
unknown <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-18 08:14:17 +0700

    menambahkan class FilmFavorite

---
+++ FilmFavorite_103022400068.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Modul7
+{
+    internal class FilmFavorite_103022400068
+    {
+        public string title { get; set; }
+        public string genre { get; set; }
+        public string director { get; set; }
+        public string year { get; set; }
+        public int rating { get; set; }
+        public int durationMinutes { get; set; }
+        public bool isWatched { get; set; }
+
+        public static void ReadJSON()
+        {
+            String file = File.ReadAllText("jurnal7_1_103022400068.json");
+            FilmFavorite_103022400068 filmFavorite = JsonSerializer.Deserialize<FilmFavorite_103022400068>(file);
+            Console.WriteLine($"Title: {filmFavorite.title} genre: {filmFavorite.genre}" +
+                $" director: {filmFavorite.director} year: {filmFavorite.year} rating {filmFavorite.rating} duration: {filmFavorite.durationMinutes} minute " +
+                $"iswatchet {filmFavorite.isWatched}");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Modul7;
+
+FilmFavorite_103022400068.ReadJSON();
